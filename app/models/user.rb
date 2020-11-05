0eec616b105da2bc5d2a4dcaabbe3b486186a432
O-DAIKI <mesh2.s.m.k@gmail.com>
2020-11-05 19:32:56 +0900

    ユーザー管理機能(指摘箇所の修正)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
     validates :first_name
     validates :last_name
   end
+
   with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
     validates :first_name_kana
     validates :last_name_kana
   end
-  validates :birthday, presence: true
+  
 end
