ae631967e819fa5941efadd9da947359ecaf5003
O-DAIKI <mesh2.s.m.k@gmail.com>
2020-11-08 14:26:27 +0900

    商品出品機能の実装(指摘箇所の修正)

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,7 +12,4 @@ class Category < ActiveHash::Base
     { id: 9, name: 'ハンドメイド' },
     { id: 10, name: 'その他' }
   ]
-
-  include ActiveHash::Associations
-  has_many :items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,11 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping_fee
-  belongs_to :shipping_from
-  belongs_to :shipping_day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :shipping_from
+  belongs_to_active_hash :shipping_day
 
   with_options presence: true do
     validates :image
